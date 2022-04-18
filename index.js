4635670ba3c1fad4994f0a76bc7eabbbfc24d4de
caiden nason <caidenbnason@gmail.com>
2022-04-18 17:55:04 -0400

    commit tree 80327c1c0d1536ad454c762ecb59a3152d0695e8

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, 2)
+}
+
+returnFirstTwoDrivers
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+}
+
+returnLastTwoDrivers
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    const multiply = function(fare){
+        return fare * num
+    }
+return multiply
+}
+
+let fareDoubler = createFareMultiplier(2)
+
+let fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(fruit, aFunction){
+    return aFunction(fruit)
+}
